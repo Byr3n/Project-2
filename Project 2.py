050cdf80e07532ae106790b6810dc3439593aaae
Byr3n <sealunion45@gmail.com>
2025-01-31 17:43:46 -0800

    first commit

---
+++ Project 2.py
@@ -0,0 +1,12 @@
+Byran="October 20th"
+print(Byran)
+Mylo="July 24th"
+print(Mylo)
+Maya="March 4th"
+print(Maya)
+Mom="December 16th"
+print(Mom)
+Dad="May 13th"
+print(Dad)
+birthday=input("what is your birthday")
+print("Nice! What a cool day",birthday,"\nmine is",Byran),
